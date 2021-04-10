650bd365ce6399ee35efb02b8d4b1517e2a937d8
Ayoub Adib <adbayb@gmail.com>
2021-04-11 01:26:38 +0200

    fix: rollback disable watch

--- src/index.ts
+++ src/index.ts
@@ -6,15 +6,15 @@ setup();
 
 const command = process.argv[2];
 
-// eslint-disable-next-line sonarjs/no-small-switch
 switch (command) {
 	case "build":
 		require("./commands/build");
 
 		break;
-	// case "watch":
-	// 	require("./commands/watch");
-	// 	break;
+	case "watch":
+		require("./commands/watch");
+
+		break;
 	default:
 		throw new ReferenceError("Command not found");
 }
