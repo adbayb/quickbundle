69962a8f125acba3618099f0203c609a8e730adc
Ayoub Adib <adbayb@gmail.com>
2021-04-10 21:35:45 +0200

    chore: remove lerna and workspaces

---
+++ scripts/postinstall-example.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Go to the project root folder and link locally the module:
+pushd ../../
+yarn link
+
+# Go back to the example folder and link quickbundle:
+popd
+yarn link quickbundle
