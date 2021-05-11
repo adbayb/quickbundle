006b449cb87d3c01db1262bdd9f4fc073fc41947
Ayoub Adib <adbayb@gmail.com>
2021-05-12 01:36:50 +0200

    feat: make build concurrent

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -4,21 +4,21 @@ import { createBundler } from "../entities/bundler";
 import { createProject } from "../entities/project";
 import { readFile, text } from "../helpers";
 
-const calculateBundleSize = async (filename: string) => {
-	const content = await readFile(filename);
-	const gzSize = await gzipSize(content);
-
-	return {
-		raw: content.byteLength,
-		gzip: gzSize,
-	};
-};
-
-const formatAllBundleSizes = async (filenames: string[]) => {
+const calculateBundleSize = async (filenames: string[]) => {
 	let output = "";
 
+	const calculateFileSize = async (filename: string) => {
+		const content = await readFile(filename);
+		const gzSize = await gzipSize(content);
+
+		return {
+			raw: content.byteLength,
+			gzip: gzSize,
+		};
+	};
+
 	for (const filename of filenames) {
-		const size = await calculateBundleSize(filename);
+		const size = await calculateFileSize(filename);
 
 		output += `📦 ${filename}\n${text(
 			size.raw.toString().padStart(11) + " B",
@@ -36,17 +36,15 @@ const main = async () => {
 	const bundle = await createBundler(project, {
 		isProduction: true,
 	});
-	const outfiles: string[] = [];
-
-	for (const target of Object.keys(project.destination) as Array<
-		"cjs" | "esm"
-	>) {
-		outfiles.push(await run(`Building ${target} 👷‍♂️`, bundle(target)));
-	}
+	const targets = Object.keys(project.destination) as Array<"esm" | "cjs">;
+	const outfiles = await run(
+		`Building ${targets.join(", ")} 👷‍♂️`,
+		Promise.all(targets.map((target) => bundle(target)))
+	);
 
 	const sizeOutput = await run<string>(
 		`Calculating bundle size 📐`,
-		formatAllBundleSizes(outfiles)
+		calculateBundleSize(outfiles)
 	);
 
 	console.info(`\n${sizeOutput}`);
