6634e0830bc594eac37272ac57f710887cc2be6b
Ayoub Adib <adbayb@gmail.com>
2023-05-07 22:25:02 +0200

    refactor: version bin proxy

---
+++ quickbundle/bin/index.cjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+
+const { join } = require("node:path");
+const pkg = require("../package.json");
+
+require(join("../", pkg.main));
--- quickbundle/src/bundler/configs/package.ts
+++ quickbundle/src/bundler/configs/package.ts
@@ -1,4 +1,4 @@
-import { resolve } from "path";
+import { resolve } from "node:path";
 import { CWD } from "../../constants";
 import { assert } from "../../helpers";
 
