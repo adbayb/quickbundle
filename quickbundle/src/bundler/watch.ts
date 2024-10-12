07e9e4952bd029496b69e0c0bbb78c2a57b94abf
Ayoub Adib <adbayb@gmail.com>
2024-10-12 13:00:38 +0200

    feat: cleaner watcher logs

--- quickbundle/src/bundler/watch.ts
+++ quickbundle/src/bundler/watch.ts
@@ -1,6 +1,4 @@
-import { relative } from "node:path";
 import { watch as rollupWatch } from "rollup";
-import type { InputOption } from "rollup";
 import { helpers } from "termost";
 
 import CONFIGURATIONS from "./config";
@@ -16,44 +14,39 @@ export const watch = () => {
 		})),
 	);
 
+	let startDuration: number;
+
 	console.clear();
 
 	watcher.on("event", async (event) => {
 		switch (event.code) {
-			case "BUNDLE_START": {
-				console.clear();
+			case "START": {
+				startDuration = Date.now();
 
-				const { input, output } = event;
-
-				helpers.message(
-					`Waiting for ${getStringifiedInputOutput(input, output)} build to be done...`,
-					{
-						type: "information",
-					},
-				);
+				clearLog(`Build in progress...`, {
+					type: "information",
+				});
 
 				return;
 			}
-			case "BUNDLE_END": {
-				console.clear();
+			case "BUNDLE_END":
+				await event.result.close();
 
-				const { duration, input, output, result } = event;
+				break;
+			case "END": {
+				const duration = Date.now() - startDuration;
 
-				await result.close();
-
-				helpers.message(
-					`Build of ${getStringifiedInputOutput(input, output)} done in ${duration}ms (at ${new Date().toLocaleTimeString()})`,
+				clearLog(
+					`Build done in ${duration}ms (at ${new Date().toLocaleTimeString()})`,
 					{ type: "success" },
 				);
 
 				return;
 			}
 			case "ERROR": {
-				console.clear();
-
 				const { error } = event;
 
-				helpers.message(`${String(error)}`, { type: "error" });
+				clearLog(`${String(error)}`, { type: "error" });
 				console.error("\n", error);
 
 				return;
@@ -64,9 +57,8 @@ export const watch = () => {
 	});
 };
 
-const getStringifiedInputOutput = (
-	input: InputOption | undefined,
-	output: readonly string[],
-) => {
-	return `${JSON.stringify(input)} ~> ${output.map((filepath) => JSON.stringify(relative(process.cwd(), filepath))).join(", ")}`;
+const clearLog = (...input: Parameters<typeof helpers.message>) => {
+	console.clear();
+
+	helpers.message(...input);
 };
