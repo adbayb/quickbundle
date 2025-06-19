bcec0eebb540a25ebce4bda2e0941a33be58ce6d
Ayoub Adib <adbayb@gmail.com>
2025-06-20 00:53:13 +0200

    feat: update dependencies

--- quickbundle/src/index.ts
+++ quickbundle/src/index.ts
@@ -1,10 +1,10 @@
 import { termost } from "termost";
 import type { Termost } from "termost";
 
-import { name, version } from "../package.json" with { type: "json" };
 import { createWatchCommand } from "./commands/watch";
 import { createCompileCommand } from "./commands/compile";
 import { createBuildCommand } from "./commands/build";
+import { name, version } from "../package.json" with { type: "json" };
 
 const createProgram = (...commandBuilders: ((program: Termost) => void)[]) => {
 	const program = termost({
